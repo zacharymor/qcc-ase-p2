70433f8f0759aeb5f61fe724f12515208df610d7
zacharymor <morzachary@gmail.com>
2024-06-03 16:40:16 -0400

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className='px-5'>
+      <Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/services">Services</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+
+function Main({ children }) {
+  return (
+    <Container className="my-5">
+      {children}
+    </Container>
+  );
+}
+
+
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <p>&copy; 2024 MyApp. All rights reserved.</p>
+    </footer>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Learn more about us on this page.</p>
+    </div>
+  );
+}
+
+function Services() {
+  return (
+    <div>
+      <h1>Services Page</h1>
+      <p>Discover our services on this page.</p>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Get in touch with us through this page.</p>
+    </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
